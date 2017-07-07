309ab7594fe6859a14c3cab0b4556bae6d81e15a
Konrad Scherer <kmscherer@gmail.com>
2017-07-07 10:25:56 -0400

    build_postprocess.sh: Missing source for setup_post_scripts function
    Signed-off-by: Konrad Scherer <kmscherer@gmail.com>

--- build_postprocess.sh
+++ build_postprocess.sh
@@ -20,6 +20,9 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
+# needed for setup_post_scripts
+source "$(dirname "$0")"/common.sh
+
 run_post_scripts()
 {
     local BUILD="$1"
